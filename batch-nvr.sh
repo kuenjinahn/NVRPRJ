fc4d2e38d0ac8e6ae3b5e35a90b78146c976495d
ahnguenjin <akj2995@gmail.com>
2025-08-17 21:42:52 +0900

    add

---
+++ batch-nvr.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# 1) conda 셸 hook 불러오기
+eval "$(conda shell.bash hook)"
+
+# 2) 환경 활성화
+conda activate beni
+# 3) nohup 백그라운드 실행
+nohup python /home/user/NVRPRj/bin/videoDataReceiver.py > /home/user/NVRPRj/receive.log 2>&1 &
+nohup python /home/user/NVRPRj/bin/videoAlertCheck.py   > /home/user/NVRPRj/alert.log   2>&1 &
+
