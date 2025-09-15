a5737a62151cb3bf63355c60bb1585031d79b24c
ahnguenjin <akj2995@gmail.com>
2025-09-15 11:06:47 +0900

    add

--- batch-nvr.sh
+++ batch-nvr.sh
@@ -8,4 +8,5 @@ conda activate nvr
 # 3) nohup 백그라운드 실행
 nohup python /home/user/NVRPRj/bin/videoDataReceiver.py > /home/user/NVRPRj/receive.log 2>&1 &
 nohup python /home/user/NVRPRj/bin/videoAlertCheck.py   > /home/user/NVRPRj/alert.log   2>&1 &
-
+nohup python /home/user/NVRPRj/bin/panorama_generator.py   > /home/user/NVRPRj/panorama_generator.log   2>&1 &
+nohup python /home/user/NVRPRj/bin/videoRecoder.py   > /home/user/NVRPRj/videoRecoder.log   2>&1 &
