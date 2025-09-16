07d7ce994b96bd9901e620f29f1b3a0f2830b5fd
ahnguenjin <akj2995@gmail.com>
2025-09-16 13:57:19 +0900

    add

--- batch-nvr.sh
+++ batch-nvr.sh
@@ -6,7 +6,7 @@ eval "$(conda shell.bash hook)"
 # 2) 환경 활성화
 conda activate nvr
 # 3) nohup 백그라운드 실행
-sudo kill -9 python
+sudo pkill -9 python
 nohup python /home/user/NVRPRj/bin/videoDataReceiver.py > /home/user/NVRPRj/receive.log 2>&1 &
 nohup python /home/user/NVRPRj/bin/videoAlertCheck.py   > /home/user/NVRPRj/alert.log   2>&1 &
 nohup python /home/user/NVRPRj/bin/panorama_generator.py   > /home/user/NVRPRj/panorama_generator.log   2>&1 &
